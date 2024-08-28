d30d357c2409f71fc283a7ccfbc8e5a83d7c4ba7
Samrajya <97937630+therealsamrajya@users.noreply.github.com>
2024-08-28 21:49:33 +0545

    Update SearchFilter.jsx

--- q2-full/q2/src/SearchFilter.jsx
+++ q2-full/q2/src/SearchFilter.jsx
@@ -36,7 +36,7 @@ const SearchFilter = () => {
     // Fetch data from the API
     const fetchData = async () => {
         try {
-            const response = await axios.get('https://q2-backend.onrender.com');
+            const response = await axios.get('https://q2-backend.onrender.com/items');
             setItems(response.data);
             setFilteredItems(response.data);
         } catch (err) {
