5fe2c4ebedf1b19f0e90535c7a5da60372ae29a2
therealsamrajya <samrajyadai@gmail.com>
2024-08-29 13:38:29 +0545

    test update

--- q2-full/q2/src/pages/home/Home.jsx
+++ q2-full/q2/src/pages/home/Home.jsx
@@ -4,7 +4,7 @@ import SearchFilter from '../../components/SearchFilter'
 const Home = () => {
   return (
     <div>
-        <span>Note : Please refresh the page 4 5 tiemes if the data is not being fetched </span>
+        <span className='text-xs'>Note : Please refresh the page 4 5 times if the data is not being fetched </span>
         <SearchFilter/>
     </div>
   )
