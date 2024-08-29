cf5c443af5d181a373fb551d281c2293f4f37637
therealsamrajya <samrajyadai@gmail.com>
2024-08-29 18:18:53 +0545

    test update

--- my-auth-app/app/page.js
+++ my-auth-app/app/page.js
@@ -1,16 +1,10 @@
 import Image from "next/image";
 import LoginPage from "./login/page";
-import DashboardPage from "./dashboard/page";
-import RegisterPage from "./register/page"
 
 export default function Home() {
   return (
-   <div>
-    <LoginPage/>
-    <DashboardPage/>
-    <RegisterPage/>
-
-
-   </div>
+    <div>
+      <LoginPage />
+    </div>
   );
 }
